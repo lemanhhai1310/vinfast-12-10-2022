6da30f8a713e5de1641e545f80e555e202e71355
hailm <hai.lemanh.1310@gmail.com>
2022-10-12 10:47:01 +0700

    init project

---
+++ dangnhap.php
@@ -0,0 +1,7 @@
+<?php $data["title"] = "Đăng nhập"; ?>
+<?php $bodyClass = '' ?>
+<?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-flex-auto ">
+
+</div>
+<?php require "template-parts/layouts/footer.php"; ?>
---
+++ template-parts/layouts/footer.php
@@ -0,0 +1,21 @@
+<div class="uk-background-muted dangnhap__footer">
+    <div class="uk-container">
+        <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+
+            <div class="uk-navbar-left uk-visible@m">
+                <a href="" class="uk-navbar-item uk-logo"><img src="images/logo-2d-footer.png" alt=""></a>
+            </div>
+
+            <div class="uk-navbar-right">
+                <div class="uk-navbar-item">
+                    <div>VinFast. All rights reserved. © Copyright 2022</div>
+                </div>
+            </div>
+
+        </nav>
+    </div>
+</div>
+</div>
+<!--/app-->
+</body>
+</html>
